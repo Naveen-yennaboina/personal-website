83b28ab6af55010316ae77e0afb7d91ec8852e21
Naveen Yennaboina <77650530+Naveen-yennaboina@users.noreply.github.com>
2023-05-21 19:30:48 +0530

    Update urls.py

--- naveenyennaboina/urls.py
+++ naveenyennaboina/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
 from education.views import Homepage
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from django.conf.urls import url, include 
 
 from django.views.static import serve
 from django.conf.urls import url
